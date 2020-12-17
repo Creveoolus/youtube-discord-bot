6befa9419cacda7c97055277614a9a9e70203574
Creveool <stepanholmi@gmail.com>
2020-12-17 21:35:41 +0100

    Update main.py

--- Rustik Bot/main.py
+++ Rustik Bot/main.py
@@ -3,6 +3,7 @@ from discord.ext import commands, tasks
 import asyncio
 import os
 import config
+from itertools import cycle
 
 intents = discord.Intents.default()
 
@@ -11,6 +12,16 @@ client = commands.Bot(command_prefix='r.', intents=intents)
 @client.event
 async def on_ready():
 	print('Бот онлайн!')
+
+	global activity
+	activity = cycle([
+	discord.Activity(type=discord.ActivityType.listening, name='А4 - KIIDS'),
+	discord.Activity(type=discord.ActivityType.watching, name='windy31'),
+	discord.Activity(type=discord.ActivityType.listening, name='Shik Music'),
+	discord.Game(name='с Discord API')
+	])
+
+	change_status.start()
 
 cogs = 0
 try:
@@ -22,5 +33,12 @@ try:
 	print(f'Всего когов загружено: {cogs}')
 except:
 	print('Когов не обнаружено!')
+
+
+@tasks.loop(seconds=7)
+async def change_status():
+	activity_ = next(activity)
+
+	await client.change_presence(status=discord.Status.dnd, activity=activity_)
 
 client.run(str(config.token))
